a8ce61ddf9748f41d8ebbd72303af42acba783df
John Collins <j.k.collins410@gmail.com>
2019-09-16 11:28:50 -0400

    Removed id from the Book model

--- db/models/Book.js
+++ db/models/Book.js
@@ -3,11 +3,7 @@ const Sequelize = require ('sequelize');
 module.exports = (sequelize) => {
   class Book extends Sequelize.Model {}
   Book.init({
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
+
         title: {
           type: Sequelize.STRING,
           allowNull: false,
@@ -34,29 +30,18 @@ module.exports = (sequelize) => {
         },
         genre: {
           type: Sequelize.STRING,
-          allowNull: false,
-          validate: {
-            notNull: {
-              msg: 'Please provide a value for "genre"'
-            },
-            notEmpty: {
-              msg: 'Please provide a value for "genre"'
-            }
-          }
+          allowNull: true,
         },
         year: {
           type: Sequelize.INTEGER,
-          allowNull: false,
+          allowNull: true,
           validate: {
-            notNull: {
-              msg: 'Please provide a value for "year"'
-            },
             min: {
               args: 1,
               msg: 'Please provide a value greater than "0" for "year"'
             }
           }
-        }
+        },
       },
 
       {sequelize});
